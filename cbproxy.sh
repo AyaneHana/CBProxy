830879f843b80d68d7dcafe136de215550ffdbe7
AyaneHana <130580666+AyaneHana@users.noreply.github.com>
2023-06-06 22:36:35 +0800

    Delete cbproxy.sh

--- cbproxy.sh
+++
@@ -1,5 +0,0 @@
-ACTUAL="snproxy"
-read -s -p "Password: " enteredpass
-echo ""
-
-[ "$enteredpass" != "$ACTUAL" ] && echo "Sorry" && echo "exiting" || pkg install openssl curl figlet -y && wget https://raw.githubusercontent.com/AyaneHana/CBProxy/main/install.sh && bash install.sh
